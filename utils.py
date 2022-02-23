c4cbbc69019d0cd00e3fdef16b282234876c1976
Andreas <andreas.oie@hotmail.com>
2022-02-23 15:46:26 +0100

    todo: fix plotting

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 def load_countries(filename: str, ignore: str):
     unavailable_countries = _load_unavailable_countries(ignore)
     countries = []
@@ -21,11 +23,18 @@ def _load_unavailable_countries(filename: str):
             unavailable_countries.append(line)
     return unavailable_countries
 
-def check_is_valid_indexing(n_samples: int):
+def check_is_valid_indexing(countries: Union[int, List[str]]):
     """ checks if the number of sampels provides sensible indexing """
-    if n_samples < 0:
-        raise ValueError("Number of countries can't be negative")
-    elif n_samples > 250:
-        raise ValueError("Doesn't exist so many countries?")
-    else:
-        return True
+    if isinstance(countries, int):
+        if countries < 0:
+            raise ValueError("Number of countries can't be negative")
+        elif countries > 250:
+            raise ValueError("Doesn't exist so many countries?")
+        else:
+            return True
+    elif isinstance(countries, list):
+        if len(countries) > 0:
+            return True
+        else:
+            raise ValueError("No countries provided")
+   
