7cd4ac768896dca29359bac98a82be8f5784ba33
Andreas <andreas.oie@hotmail.com>
2022-02-23 13:47:05 +0100

    added utils

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
-
-def load_countries(filename):
+def load_countries(filename: str, ignore: str):
+    unavailable_countries = _load_unavailable_countries(ignore)
     countries = []
     with open(filename, "r") as f:
         lines = f.readlines()
@@ -8,5 +8,24 @@ def load_countries(filename):
             line_list = line.split(",")
             code = line_list[0]
             country = line_list[1]
-            countries.append((code, country))
+            if country not in unavailable_countries:
+                countries.append((code, country))
     return countries
+
+def _load_unavailable_countries(filename: str):
+    unavailable_countries = []
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            unavailable_countries.append(line)
+    return unavailable_countries
+
+def check_is_valid_indexing(n_samples: int):
+    """ checks if the number of sampels provides sensible indexing """
+    if n_samples < 0:
+        raise ValueError("n_samples can't be negative")
+    elif n_samples == 1:
+        raise ValueError("n_samples can't be 1")
+    else:
+        return True
